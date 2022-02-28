abe64c127a671bf6386e4a5ed66545b3608b5cb4
raazs3311 <sraaz5948@gmail.com>
2022-02-28 11:57:34 +0530

    changes

--- electro/electro/urls.py
+++ electro/electro/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-admin.site.site_header = 'My admin'
+admin.site.site_header = 'Ecom admin'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('acces.urls')),
